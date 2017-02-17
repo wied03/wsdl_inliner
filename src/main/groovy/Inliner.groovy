b4cd01bacbf01787def8288981d582625e26cde5
Brady Wied <brady@wied.us>
2017-02-17 11:47:08 -0500

    clarify

--- src/main/groovy/Inliner.groovy
+++ src/main/groovy/Inliner.groovy
@@ -9,13 +9,14 @@ class Inliner {
         'rm -rf tmp'.execute().waitFor()
         'rm -rf stuff.wsdl'.execute().waitFor()
         // TODO: Clean out the tmp directory
-        WSDLToJava.main('-d', 'tmp',"/Users/brady/code/eclipse_workspaces/mule/studio621/soaptesting/src/main/wsdl/test.wsdl")
-        def filenames = new FileNameFinder().getFileNames('tmp', '**/*.java').join ' '
+        WSDLToJava.main('-d', 'tmp',
+                        "/Users/brady/code/eclipse_workspaces/mule/studio621/soaptesting/src/main/wsdl/test.wsdl")
+        def filenames = new FileNameFinder().getFileNames('tmp', '**/*.java')
         // javatows needs compiled code to re-generate the WSDL
         // TODO: Log this instead
-        println "compiling ${filenames}"
+        println "compiling ${filenames.join ' '}"
         "javac ${filenames}".execute().waitFor()
-        // TODO: Have to find out what the interface is automatically
+        // TODO: Have to find out what the interface is automatically (grep files for the annotation)
         // inlines schema by default
         // TODO: Stuff.wsdl should go into a generated resources directory using the Maven Mojo
         JavaToWS.main('-wsdl', '-cp', 'tmp', '-o', 'stuff.wsdl', "com.quintiles.services.soaptesting.v1.SOAPTest")
