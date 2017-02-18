0ee9c4ab76911c08796533d9d1b209082cacc3e5
Brady Wied <brady@wied.us>
2017-02-17 18:39:24 -0700

    Tweaks/expected output

--- src/main/groovy/com/bswtechconsulting/wsdl/Inliner.groovy
+++ src/main/groovy/com/bswtechconsulting/wsdl/Inliner.groovy
@@ -10,14 +10,19 @@ class Inliner {
     // will return the name of the service and port since CXF might rename them from the original WSDL
     static WsdlData inline(File inputWsdl, File outputWsdl) {
         File.createTempDir().with {
-            println "using tmp dir ${absolutePath}"
             deleteOnExit()
             WSDLToJava.main('-d',
                             absolutePath,
                             inputWsdl.absolutePath)
             def filenames = new FileNameFinder().getFileNames(absolutePath, '**/*.java')
             // javatows needs compiled code to re-generate the WSDL
-            "javac ${filenames}".execute().waitFor()
+            // TODO: Windows paths/javac location
+            def command = "javac ${filenames.join(' ')}"
+            def result = command.execute()
+            def exitValue = result.waitFor()
+            if (exitValue != 0) {
+                throw new Exception("Compilation error ${result.text}")
+            }
             // TODO: Have to find out what the interface is automatically (grep files for the annotation)
             // inlines schema by default
             outputWsdl.parentFile.mkdirs()
